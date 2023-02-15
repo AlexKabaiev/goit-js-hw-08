535852fc1a4582ab0eb640a68937edbb7c00eac3
Alexey Kabaiev <kabaievalex@gmail.com>
2023-02-15 13:47:28 +0200

    Task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryPreview = document.querySelector('.gallery');
+galleryPreview.innerHTML = galleryItems.reduce(
+  (html, current) =>
+    html +
+    `<a class="gallery__item" href="${current.original}">
+  <img 
+  class="gallery__image" 
+  src="${current.preview}" 
+  alt="${current.description}" />
+</a>`,
+  ''
+);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
